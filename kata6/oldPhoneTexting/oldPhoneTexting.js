be1cd91918b2be4d520c86709c76454acf20792b
RJ Salamanca <salamancaronito@gmail.com>
2019-05-12 01:20:36 -0400

    90% completed solution, need to find work arounds for non alpha chars

--- kata6/oldPhoneTexting/oldPhoneTexting.js
+++ kata6/oldPhoneTexting/oldPhoneTexting.js
@@ -1,14 +1,59 @@
 function sendMessage(message){
-    console.log(message);
-    // Start typing here
-    return message;
+    const phoneLayout = {
+        '1':'.,?!',
+        '2':'abc',
+        '3':'def',        
+        '4':'ghi',
+        '5':'jkl',
+        '6':'mno',        
+        '7':'qprs',
+        '8':'tuv',
+        '9':'wxyz',        
+        '0':' ',
+        '*':'\'-+=',
+    }
+
+    message = message.split('').map((letter,index) => {
+        let buttonClicks = '';
+        let changedCase = false;
+
+        if(letter == ' '){
+            buttonClicks += ' ';  
+        } else if(letter == letter.toUpperCase()){
+            buttonClicks += '#';
+            letter = letter.toLowerCase();
+            changedCase = true;
+        }
+
+        for(key in phoneLayout){
+            const timesPressed = phoneLayout[key].indexOf(letter);
+            const lastPressed = phoneLayout[key].indexOf(message[index-1])
+
+            if(timesPressed != -1 && lastPressed != -1){
+                buttonClicks += ' '
+            }
+            if(timesPressed != -1){
+                for(let i = 0; i <= timesPressed; i++){
+                    buttonClicks += key
+                }
+            }
+        }
+
+        if(changedCase){
+            buttonClicks += '#'
+        }
+        console.log(`Letter: ${letter} Clicks: ${buttonClicks}`)
+
+        return buttonClicks;
+    });
+    return message.join('');
 }
 
 
 // TESTING
-sendMessage('hey') //4433999
-//   ["one two three", "666 6633089666084477733 33"],
-//   ["Hello World!", "#44#33555 5556660#9#66677755531111"],
+console.log(sendMessage('heyy ')); //4433999
+console.log(sendMessage('one two three')); // "666 6633089666084477733 33"
+console.log(sendMessage('Hello World!')); //"#44#33555 5556660#9#66677755531111"
 //   ["Def Con 1!", "#3#33 3330#222#666 6601-1111"],
 //   ["A-z", "#2**#9999"],
 //   ["1984", "1-9-8-4-"],
